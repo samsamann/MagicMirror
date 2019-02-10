e0dfbc69d07efc86ce4bd8fa474bf7cd8f9be24b
Samuel Ackermann <saemi.ackermann@gmail.com>
2019-02-10 14:08:21 +0100

    Remove unused files in root module

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,9 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: ''
 })
-export class AppComponent {
-  title = 'magic-mirror-frontend';
-}
+export class AppComponent {}
